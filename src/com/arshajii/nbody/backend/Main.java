407c14ad107cafd39507c47e8e227a9340e43d37
A. R. Shajii <ariya.r.shajii@gmail.com>
2013-12-03 16:00:23 -0500

    Make Main Java 6 compliant.

--- src/com/arshajii/nbody/backend/Main.java
+++ src/com/arshajii/nbody/backend/Main.java
@@ -7,14 +7,13 @@ import java.io.PrintWriter;
 // TEST
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Body sun = new StaticBody(new Vec(0, 0), new Vec(0, 0),
+		Body sun = new StaticBody(new Vec(0, 0), Const.SOLAR_MASS);
+
+		Body earth = new Body(new Vec(149600000000d, 0), new Vec(0, 30000d),
+				5.972E24);
+
+		Body sun2 = new StaticBody(new Vec(0, -149600000000d * 2),
 				Const.SOLAR_MASS);
-
-		Body earth = new Body(new Vec(149_600_000_000d, 0),
-				new Vec(0, 30_000d), 5.972E24);
-
-		Body sun2 = new StaticBody(new Vec(0, -149_600_000_000d * 2), new Vec(
-				0, 0), Const.SOLAR_MASS);
 
 		Universe u = new Universe(new Body[] { sun, sun2, earth });
 
