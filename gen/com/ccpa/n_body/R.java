c25805d4085f3ff79e531dfbd58d52a03ac76f04
admiralFedora <512.quadraphonic@gmail.com>
2013-12-08 15:07:05 -0500

    Cleaned up menu and settings layout

--- gen/com/ccpa/n_body/R.java
+++ gen/com/ccpa/n_body/R.java
@@ -10,6 +10,10 @@ package com.ccpa.n_body;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background_color=0x7f040000;
+        public static final int text_color=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,56 +21,60 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int about_button=0x7f020000;
-        public static final int dark_background=0x7f020001;
-        public static final int enter_simulation2=0x7f020002;
-        public static final int flat_background=0x7f020003;
-        public static final int get_started_button=0x7f020004;
-        public static final int get_started_page=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int menu_screen=0x7f020007;
-        public static final int new_setting2=0x7f020008;
-        public static final int setting=0x7f020009;
-        public static final int tutorial_button=0x7f02000a;
-        public static final int welcome_screen=0x7f02000b;
+        public static final int back=0x7f020001;
+        public static final int dark_background=0x7f020002;
+        public static final int enter_simulation2=0x7f020003;
+        public static final int flat_background=0x7f020004;
+        public static final int get_started_button=0x7f020005;
+        public static final int get_started_page=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int menu_screen=0x7f020008;
+        public static final int new_setting2=0x7f020009;
+        public static final int next=0x7f02000a;
+        public static final int orbit=0x7f02000b;
+        public static final int setting=0x7f02000c;
+        public static final int tutorial_button=0x7f02000d;
+        public static final int welcome_screen=0x7f02000e;
     }
     public static final class id {
-        public static final int AbsoluteLayout1=0x7f08000a;
-        public static final int Button01=0x7f08000f;
-        public static final int Button02=0x7f080010;
-        public static final int Button03=0x7f080011;
-        public static final int TextView01=0x7f080006;
-        public static final int about=0x7f08000e;
-        public static final int action_settings=0x7f08001b;
-        public static final int back_main=0x7f08001e;
-        public static final int body_opt_mass=0x7f080005;
-        public static final int body_opt_size=0x7f080003;
-        public static final int button1=0x7f080007;
-        public static final int clear=0x7f08001c;
-        public static final int compassInfo=0x7f080000;
-        public static final int gravConstant=0x7f080014;
-        public static final int latitute=0x7f080001;
-        public static final int longitude=0x7f080002;
-        public static final int massSize=0x7f080017;
-        public static final int myGLSurfaceView1=0x7f080012;
-        public static final int pause=0x7f08001d;
-        public static final int progressBar1=0x7f080008;
-        public static final int randColor=0x7f08001f;
-        public static final int settings=0x7f08000d;
-        public static final int start=0x7f08000b;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080009;
-        public static final int textView3=0x7f080013;
-        public static final int textView4=0x7f080016;
-        public static final int textView5=0x7f080018;
-        public static final int textView6=0x7f080019;
-        public static final int tutorial=0x7f08000c;
-        public static final int uniformColor=0x7f08001a;
-        public static final int uniformSize=0x7f080015;
+        public static final int AbsoluteLayout1=0x7f09000a;
+        public static final int Button01=0x7f090010;
+        public static final int Button02=0x7f090011;
+        public static final int Button03=0x7f090012;
+        public static final int TextView01=0x7f090006;
+        public static final int about=0x7f09000e;
+        public static final int action_settings=0x7f09001c;
+        public static final int back_main=0x7f09001f;
+        public static final int body_opt_mass=0x7f090005;
+        public static final int body_opt_size=0x7f090003;
+        public static final int button1=0x7f090007;
+        public static final int clear=0x7f09001d;
+        public static final int compassInfo=0x7f090000;
+        public static final int gravConstant=0x7f090015;
+        public static final int imageView1=0x7f09000f;
+        public static final int latitute=0x7f090001;
+        public static final int longitude=0x7f090002;
+        public static final int massSize=0x7f090018;
+        public static final int myGLSurfaceView1=0x7f090013;
+        public static final int pause=0x7f09001e;
+        public static final int progressBar1=0x7f090008;
+        public static final int randColor=0x7f090020;
+        public static final int settings=0x7f09000d;
+        public static final int start=0x7f09000b;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090009;
+        public static final int textView3=0x7f090014;
+        public static final int textView4=0x7f090017;
+        public static final int textView5=0x7f09001a;
+        public static final int textView6=0x7f090019;
+        public static final int tutorial=0x7f09000c;
+        public static final int uniformColor=0x7f09001b;
+        public static final int uniformSize=0x7f090016;
     }
     public static final class layout {
         public static final int activity_compass__mode=0x7f030000;
@@ -82,16 +90,16 @@ public final class R {
         public static final int simulation_fullscreen=0x7f03000a;
     }
     public static final class menu {
-        public static final int compass__mode=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int compass__mode=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int clear=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_compass__mode=0x7f050004;
-        public static final int title_activity_open_gl=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int clear=0x7f060005;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_compass__mode=0x7f060004;
+        public static final int title_activity_open_gl=0x7f060003;
     }
     public static final class style {
         /** 
@@ -115,10 +123,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
